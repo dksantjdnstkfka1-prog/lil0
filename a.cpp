4f24eebbce8d38029bf07f5fbf679bcf664c122c
윤인상 <dksantjdnstkfka1@gmail.com>
2026-04-09 10:33:29 +0900

    첫 번째 커밋 메시지

---
+++ a.cpp
@@ -0,0 +1,8 @@
+total 53
+drwxr-xr-x 1 YUHAN 197121     0 Apr  9 10:26 ./
+drwxr-xr-x 1 YUHAN 197121     0 Apr  9 09:41 ../
+drwxr-xr-x 1 YUHAN 197121     0 Apr  9 10:20 .git/
+-rw-r--r-- 1 YUHAN 197121  7788 Apr  9 09:42 .gitignore
+-rw-r--r-- 1 YUHAN 197121 35823 Apr  9 09:42 LICENSE
+-rw-r--r-- 1 YUHAN 197121    37 Apr  9 10:17 README.md
+-rw-r--r-- 1 YUHAN 197121     0 Apr  9 10:26 a.cpp
